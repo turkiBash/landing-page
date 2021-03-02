87269fbfaadd98f0a5f3108c772f00bd207a5e2e
Turki <dev@turki.tech>
2021-03-03 01:28:10 +0300

    First commit

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Header = () => {
+
+    return(
+
+        <div className="top-header">
+            <header>
+                Turki
+            </header>
+
+        </div>
+    )
+}
+
+export default Header
