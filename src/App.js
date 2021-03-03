3ee4c99b16b1830cc4feb6c0d6bb6d257e63108d
Turki <dev@turki.tech>
2021-03-03 15:47:19 +0300

    Added a header and logo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
-import Header from "./components/Header"
+import Header from "./components/Header";
+
 
 
 function App() {
   return (
     <div className="App">
+        <img/>
       <Header />
     </div>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,20 @@
 import React from 'react'
+import logo from '../images/logo.svg'
+
+
 
 const Header = () => {
 
     return(
 
         <div className="top-header">
+
             <header>
-                Turki
+                <img className="logo"src={logo} />
+                <li>Home</li>
+                <li>Language</li>
+                <li>Login</li>
+                <li>Register</li>
             </header>
 
         </div>
